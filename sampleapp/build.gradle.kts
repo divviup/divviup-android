309abfc043f0255e3d32cde69bb5c79fef31696e
David Cook <dcook@divviup.org>
2023-11-01 18:08:35 -0500

    Update target SDK version for sample app

--- sampleapp/build.gradle.kts
+++ sampleapp/build.gradle.kts
@@ -9,7 +9,7 @@ android {
     defaultConfig {
         applicationId = "org.divviup.sampleapp"
         minSdk = 21
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
