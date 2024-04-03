9b1e4baf0878572700da014c12b7f05259120625
Ameer Ghani <inahga@gmail.com>
2024-04-03 12:06:47 -0400

    Update to version 0.2.0 (#133)

--- divviup/build.gradle.kts
+++ divviup/build.gradle.kts
@@ -18,7 +18,7 @@ android {
     defaultConfig {
         minSdk = 21
 
-        version = "0.2.0-SNAPSHOT"
+        version = "0.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
