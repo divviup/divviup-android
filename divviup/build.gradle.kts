a953d83afbfafe0fa98bd882fabc1da57abecb4c
Ameer Ghani <inahga@divviup.org>
2024-04-03 18:05:00 -0400

    Update version to 0.2.1-SNAPSHOT

--- divviup/build.gradle.kts
+++ divviup/build.gradle.kts
@@ -18,7 +18,7 @@ android {
     defaultConfig {
         minSdk = 21
 
-        version = "0.2.0"
+        version = "0.2.1-SNAPSHOT"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
