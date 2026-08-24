f7e3cd0d4aa572382d9d58f9b179807206a623fb
David Cook <dcook@divviup.org>
2026-08-24 13:02:55 -0500

    Bump sourceCompatibility and targetCompatibility

--- divviup/build.gradle.kts
+++ divviup/build.gradle.kts
@@ -37,8 +37,8 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_1_9
+        targetCompatibility = JavaVersion.VERSION_1_9
     }
 
     publishing {
